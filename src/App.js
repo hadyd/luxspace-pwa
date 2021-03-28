c0e394eb93c3a264e167d339428c8aaf838fe430
hadyd <anr.hadyd@gmail.com>
2021-03-28 15:17:38 +0700

    Convert HTML to React Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Arrived from './components/Arrived';
+import Browse from './components/Browse';
+import Client from './components/Client';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Header />
+<Hero />
+<Browse />
+<Arrived />
+<Client />
+<AsideMenu />
+<Footer />
+</>
   );
 }
 
